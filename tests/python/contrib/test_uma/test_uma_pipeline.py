79ed701409bfb15fcb96ec722a90962034c5f5b4
Michael J. Klaiber <michael.klaiber@de.bosch.com>
2022-07-11 17:53:04 +0000

    [UMA] added conv2d test

--- tests/python/contrib/test_uma/test_uma_pipeline.py
+++ tests/python/contrib/test_uma/test_uma_pipeline.py
@@ -15,52 +15,169 @@
 # specific language governing permissions and limitations
 # under the License.
 from os.path import join
+from typing import Union, List, Dict, Any
 
 import pytest
+from tests.python.relay.aot.test_crt_aot_usmp import (
+    _check_for_no_tvm_backendallocworkspace_calls,
+    MOBILENET_V1_URL,
+)
+from tvm.contrib.download import download_testdata
 from tvm.micro.testing.aot_test_utils import AOT_DEFAULT_RUNNER
-
-from tvm.testing.aot import run_and_check, AOTTestModel, AOTCompiledTestModel, AOTTestRunner
+from tvm.relay import transform
+from tvm.testing.aot import (
+    run_and_check,
+    AOTTestModel,
+    AOTCompiledTestModel,
+    AOTTestRunner,
+    create_relay_module_and_inputs_from_tflite_file,
+    generate_ref_data,
+    compile_and_run,
+)
 
 import tvm
-#from tvm.relay.backend.contrib.uma._template.backend import MyAiHwBackend
 from test_uma_vanilla_accelerator import VanillaAcceleratorBackend
 from tvm import relay, IRModule
-from tvm.contrib.download import download_testdata
 import numpy as np
 from collections import OrderedDict
-import tarfile
-from pathlib import Path
+from tvm.micro import model_library_format as mlf
+
 import onnx
+from tvm.testing.aot import compile_models
 
-from tvm.contrib import utils
+
+@pytest.mark.parametrize(
+    "interface_api,use_unpacked_api,test_runner,groups,weight_shape",
+    [("c", True, AOT_DEFAULT_RUNNER, 1, 32)],
+)
+def test_conv2d(interface_api, use_unpacked_api, test_runner, groups, weight_shape):
+    """Test a subgraph with a single conv2d operator."""
+    dtype = "float32"
+    ishape = (1, 32, 14, 14)
+    wshape = (32, weight_shape, 3, 3)
+    pass_config = {"tir.usmp.enable": True}
+    test_runner = AOTTestRunner(
+        makefile=test_runner.makefile,
+        prologue=test_runner.prologue,
+        epilogue=test_runner.epilogue,
+        includes=test_runner.includes,
+        parameters=test_runner.parameters,
+        pass_config=pass_config,
+    )
+
+    data0 = relay.var("data", shape=ishape, dtype=dtype)
+    weight0 = relay.var("weight", shape=wshape, dtype=dtype)
+    out = relay.nn.conv2d(data0, weight0, kernel_size=(3, 3), padding=(1, 1), groups=groups)
+    main_f = relay.Function([data0, weight0], out)
+    mod = tvm.IRModule()
+    mod["main"] = main_f
+    mod = transform.InferType()(mod)
+
+    i_data = np.random.uniform(0, 1, ishape).astype(dtype)
+    w1_data = np.random.uniform(0, 1, wshape).astype(dtype)
+
+    inputs = OrderedDict([("data", i_data), ("weight", w1_data)])
+
+    output_list = generate_ref_data(mod, inputs)
+
+    uma_backend = VanillaAcceleratorBackend()
+    uma_backend.register()
+    mod = uma_backend.partition(mod)
+    target = tvm.target.Target("vanilla_accelerator", host=tvm.target.Target("c"))
+
+    compile_and_run(
+        AOTTestModel(module=mod, inputs=inputs, outputs=output_list),
+        test_runner,
+        interface_api,
+        use_unpacked_api,
+        target=target,
+    )
+    compiled_test_mods = compile_models(
+        models=AOTTestModel(module=mod, inputs=inputs, outputs=output_list),
+        interface_api=interface_api,
+        use_unpacked_api=use_unpacked_api,
+        pass_config=test_runner.pass_config,
+        target=target,
+    )
+
+    for compiled_model in compiled_test_mods:
+        _check_for_no_tvm_backendallocworkspace_calls(compiled_model.executor_factory.lib)
+
+    run_and_check(
+        models=compiled_test_mods,
+        runner=test_runner,
+        interface_api=interface_api,
+    )
 
 
-# def import_mnist12() -> [IRModule, dict]:
-#     model_url = "".join(
-#         ["https://github.com/onnx/models/raw/main/vision/classification/mnist/model/mnist-12.onnx"])
-#     model_path = download_testdata(model_url, "mnist-12.onnx", module="onnx")
-#     onnx_model = onnx.load(model_path)
-#     input_name = "Input3"
-#     shape_dict = {input_name: (1, 1, 28, 28)}
-#     mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
-#     return mod, params
-#
-#
-# def import_restnet50() -> [IRModule, dict]:
-#     model_url = "".join(
-#         ["https://github.com/onnx/models/raw/main/vision/classification/resnet/model/resnet50-v2-7.onnx"])
-#     model_path = download_testdata(model_url, "resnet50-v2-7.onnx", module="onnx")
-#     onnx_model = onnx.load(model_path)
-#     input_name = "data"
-#     shape_dict = {input_name: (1, 3, 224, 224)}
-#     mod, params = relay.frontend.from_onnx(onnx_model, shape_dict)
-#     return mod, params
+def test_tflite_model_u1_usecase(model_url, usmp_algo, workspace_size, constant_size):
+    """
+    This checks for ML models and the memory used by them
+    when using USMP with different algorithms
+    """
+    pytest.importorskip("tflite")
+
+    import tvm.relay.testing.tf as tf_testing  # pylint: disable=import-outside-toplevel
+
+    use_unpacked_api = True
+    interface_api = "c"
+    test_runner = AOTTestRunner(
+        pass_config={"tir.usmp.enable": True, "tir.usmp.algorithm": usmp_algo}
+    )
+
+    tflite_model_file = tf_testing.get_workload_official(
+        model_url[0],
+        model_url[1],
+    )
+    mod, inputs, params = create_relay_module_and_inputs_from_tflite_file(tflite_model_file)
+    output_list = generate_ref_data(mod, inputs, params)
+
+    uma_backend = VanillaAcceleratorBackend()
+    uma_backend.register()
+    mod = uma_backend.partition(mod)
+    target = tvm.target.Target("vanilla_accelerator", host=tvm.target.Target("c"))
+
+    aotmodel = AOTTestModel(module=mod, inputs=inputs, outputs=output_list, params=params)
+    compiled_test_mods = compile_models(
+        aotmodel,
+        interface_api=interface_api,
+        use_unpacked_api=use_unpacked_api,
+        pass_config=test_runner.pass_config,
+        target=target,
+    )
+
+    for compiled_model in compiled_test_mods:
+        _check_for_no_tvm_backendallocworkspace_calls(compiled_model.executor_factory.lib)
+
+    # Checking the workspace size reported in model library format
+    mlf_memory_map = mlf._build_function_memory_map(
+        compiled_test_mods[0].executor_factory.function_metadata
+    )
+    assert mlf_memory_map["main"][0]["workspace_size_bytes"] == workspace_size
+    assert mlf_memory_map["main"][0]["constants_size_bytes"] == constant_size
+    # That should match to workspace size that will be codegen'd to the entry point.
+    allocated_pool_info_size = sum(
+        [
+            _.allocated_size
+            for _ in list(
+                dict(
+                    compiled_test_mods[0].executor_factory.executor_codegen_metadata.pool_inputs
+                ).values()
+            )
+        ]
+    )
+    assert allocated_pool_info_size == workspace_size + constant_size
+
+    run_and_check(
+        models=compiled_test_mods, runner=test_runner, interface_api=interface_api, target=target
+    )
 
 
 def download_and_import_onnx_model(model_url: str) -> [IRModule, dict, dict, dict]:
     """
     Download an ONNX NN model from `url`  and import it using the TVM onnx frontend
     """
+
     def _get_shape(io):
         shape = []
         dimensions = io.type.tensor_type.shape.dim
@@ -86,26 +203,23 @@ def download_and_import_onnx_model(model_url: str) -> [IRModule, dict, dict, dic
     return mod, params, input_shape_dict, output_shape_dict
 
 
+def test_vanilla_accelerator_integration(url: str):
+    mod, params, input_shapes, output_shapes = download_and_import_onnx_model(url)
+    _vanilla_accelerator_run(mod, params, input_shapes, output_shapes)
+
+
 def _vanilla_accelerator_run(mod, params, input_shapes, output_shapes):
+    interface_api = "c"
+    use_unpacked_api = True
+
     uma_backend = VanillaAcceleratorBackend()
     uma_backend.register()
     mod = uma_backend.partition(mod)
-
     target = tvm.target.Target("vanilla_accelerator", host=tvm.target.Target("c"))
-    generic_target = tvm.target.Target("c")
-    runtime = tvm.relay.backend.Runtime("crt")
-    executor = tvm.relay.backend.Executor("aot", {"workspace-byte-alignment": 8})
 
-    with tvm.transform.PassContext(
-        opt_level=3,
-        config={"tir.disable_vectorize": True,
-                "tir.usmp.enable": True,
-                "tir.usmp.algorithm": "greedy_by_conflicts"},
-        disabled_pass=["AlterOpLayout"]
-    ):
-        relay_mod = relay.build(mod, target=[generic_target, target], runtime=runtime, executor=executor, params=params)
-
-    def _generate_rt_data(input_shapes: dict, output_shapes: dict) -> [OrderedDict, OrderedDict]:
+    def _generate_runtime_data(
+        input_shapes: dict, output_shapes: dict
+    ) -> [OrderedDict, OrderedDict]:
         assert len(input_shapes) == 1
         assert len(output_shapes) == 1
 
@@ -118,63 +232,55 @@ def _vanilla_accelerator_run(mod, params, input_shapes, output_shapes):
         inputs = OrderedDict([(iname, i_data)])
         outputs = OrderedDict([(oname, o_data)])
         return inputs, outputs
-    input_list, output_list = _generate_rt_data(input_shapes, output_shapes)
-    model = AOTTestModel(module=mod, inputs=input_list, outputs=output_list)
-    compiled_model = [AOTCompiledTestModel(model=model, executor_factory=relay_mod)]
+
+    input_list, output_list = _generate_runtime_data(input_shapes, output_shapes)
+    aot_test_model = AOTTestModel(module=mod, inputs=input_list, outputs=output_list)
+    test_runner = AOT_DEFAULT_RUNNER
+
+    compile_and_run(aot_test_model, test_runner, interface_api, use_unpacked_api, target=target)
+
+    compiled_test_mods = compile_models(
+        models=AOTTestModel(module=mod, inputs=input_list, outputs=output_list),
+        interface_api=interface_api,
+        use_unpacked_api=use_unpacked_api,
+        pass_config=test_runner.pass_config,
+        target=target,
+    )
+
+    for compiled_model in compiled_test_mods:
+        _check_for_no_tvm_backendallocworkspace_calls(compiled_model.executor_factory.lib)
+
+    run_and_check(
+        models=compiled_test_mods,
+        runner=test_runner,
+        interface_api=interface_api,
+    )
+
+    aot_test_model = AOTTestModel(module=mod, inputs=inputs, outputs=output_list, params=params)
+
     runner = AOTTestRunner(pass_config={"tir.usmp.enable": True})
-    run_and_check(compiled_model, runner, "packed")
 
-    # temp = utils.tempdir(keep_for_debug=True)
-    # model_library_format_tar_path = Path(join(temp.path, "build", "lib.tar"))
-    # model_library_format_tar_path.unlink(missing_ok=True)
-    # model_library_format_tar_path.parent.mkdir(parents=True, exist_ok=True)
-    # print(model_library_format_tar_path.absolute())
-    #
-    # tvm.micro.export_model_library_format(module, model_library_format_tar_path)
-    #
-    # print("Built MLF Library: ")
-    # with tarfile.open(model_library_format_tar_path, "r:*") as tar_f:
-    #     print("\n".join(f" - {m.name}" for m in tar_f.getmembers()))
-    #     tar_f.extractall(model_library_format_tar_path.parent)
+    compiled_test_mods = compile_models(
+        aot_test_model,
+        interface_api=interface_api,
+        use_unpacked_api=use_unpacked_api,
+        pass_config=runner.pass_config,
+        target=target,
+    )
 
+    for compiled_model in compiled_test_mods:
+        _check_for_no_tvm_backendallocworkspace_calls(compiled_model.executor_factory.lib)
 
+    mlf_memory_map = mlf._build_function_memory_map(
+        compiled_test_mods[0].executor_factory.function_metadata
+    )
 
-@pytest.mark.parametrize(
-    "url",
-    ["https://github.com/onnx/models/raw/main/vision/classification/resnet/model/resnet50-v2-7.onnx",
-     "https://github.com/onnx/models/raw/main/vision/classification/mnist/model/mnist-12.onnx"
-     ]
-)
-def test_vanilla_accelerator_integration(url: str):
-    mod, params, input_shapes, output_shapes = download_and_import_onnx_model(url)
-    _vanilla_accelerator_run(mod, params, input_shapes, output_shapes)
-
-# def test_single_layer():
-#     x0 = relay.var("input")
-#     x1 = relay.var("param0")
-#     x1 = relay.var("param0")
-#     y = relay.nn.dense(x0, x1, units=256)
-#
-#     RELAY_MODEL = """
-#     #[version = "0.0.5"]
-#     def @main(
-#         %input_1:   Tensor[(1, 1, 28, 28), float32],
-#         %v_param_1: Tensor[(256, 784), float32],
-#         %v_param_2: Tensor[(256), float32]
-#         )
-#     {
-#       %0 = nn.batch_flatten(%input_1);
-#       %1 = nn.dense(%0, %v_param_1, units=256);
-#       %2 = nn.bias_add(%1, %v_param_2);
-#       nn.relu(%2)
-#     }
-#     """
-#     mod = tvm.parser.fromtext(RELAY_MODEL)
+    run_and_check(compiled_test_mods, runner, interface_api)
 
 
 if __name__ == "__main__":
     test_vanilla_accelerator_integration(
-        "https://github.com/onnx/models/raw/main/vision/classification/mnist/model/mnist-12.onnx")
-
-
-
+        "https://github.com/onnx/models/raw/main/vision/classification/mnist/model/mnist-12.onnx"
+    )
+    # test_tflite_model_u1_usecase(MOBILENET_V1_URL, "greedy_by_size", 4845696, 8468008)
+    # test_conv2d("c", True, AOT_DEFAULT_RUNNER, 1, 32)
